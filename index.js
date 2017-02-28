4a6246ce9694ef6a462cf67e504a74422f086e5b
Carlos Eduardo Panarello <carlospanarello@gmail.com>
2017-02-28 10:06:27 -0300

    Update index.js
    request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('sync-request');
+const request = require('request');
 
 const restService = express();
 
