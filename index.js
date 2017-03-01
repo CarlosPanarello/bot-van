bbef8425007e6e98607fb00ad9873df7f8bc6f71
Carlos Eduardo Panarello <carlospanarello@gmail.com>
2017-03-01 09:05:42 -0300

    Update index.js
    Retirando código desnecessário e colocando outra funcionalidade de busca de locais

--- index.js
+++ index.js
@@ -2,102 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
 const syncrequest = require('sync-request');
 
 const restService = express();
 
 restService.use(bodyParser.json());
 
-function synchAPICalls (url) {
-  
-  http.get(url,function(res){
-    var chunks = '';
-    
-    res.on('data',function(d){
-      chunk += d;
-    });
-    
-    res.on('end',function(){
-      results.push(chunks);
-      return results;
-    });
-  });
-}
-
-const getContent = function(url) {
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-    // select http or https module, depending on reqested url
-    const lib = url.startsWith('https') ? require('https') : require('http');
-    const request = lib.get(url, (response) => {
-      // handle http errors
-      if (response.statusCode < 200 || response.statusCode > 299) {
-         reject(new Error('Failed to load page, status code: ' + response.statusCode));
-       }
-      // temporary data holder
-      const body = [];
-      // on every content chunk, push it to the data array
-      response.on('data', (chunk) => body.push(chunk));
-      // we are done, resolve promise with those joined chunks
-      response.on('end', () => resolve(body.join('')));
-    });
-    // handle connection errors of the request
-    request.on('error', (err) => reject(err));
-    });
-};
-
-function acessoAPIVan(idOri,idDest,callback){
-    var headers = {
-        'User-Agent':       'Super Agent/0.0.1',
-        'Content-Type':     'application/x-www-form-urlencoded'
-    };
-
-    // Configure the request
-    var options = {
-        uri: 'https://vans.labbs.com.br/horario',                                
-        method: 'GET',
-        headers: headers,                                
-        qs: {'idOrigem': idOri, 'idDestino': idDest}
-    };
-  
-    
-    // Start the request
-    request(options, function (error, response, body) {
-        var speech = 'Não foi possível obter os horarios.0';
-
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            var info = JSON.parse(body);
-            var horariosVans = '';
-            console.log('REQUEST->'+body);
-            console.log('Info->'+info);
-            console.log('Info-Length->'+info.length);
-
-            for(var i = 0; i < info.length; i++) {
-                horariosVans +=info[i]+ ' ';
-            }
-            console.log('Horarios->' + horariosVans);
-            if ( horariosVans.length > 0){
-                //speech += 'Horarios da van entre ' + descOrigem + ' e '+ descDestino + ' são: '+ horariosVans.trim() + '.';                                  
-                speech = horariosVans+ '.';                                    
-            } else {
-                speech = 'Não foi possível obter os horarios.1';
-            }
-        }else{
-            speech = 'Não foi possível obter os horarios.2';
-            console.log('ERROR->'+error);
-            console.log('status->'+response.statusCode);
-        }
-        console.log('dentro do request speech->'+speech);
-        callback(speech);
-      
-    });
-}
-
 var retornaCodigo = function(entrada){
     entrada = entrada.toUpperCase();
-    console.log('Entrada->' + entrada);
+    console.log('retornaCodigo Entrada->' + entrada);
     switch(entrada) {
         case "SEDE III":
             return '1';
@@ -139,70 +52,60 @@ restService.post('/hook', function (req, res) {
                     speech += ' ';
                 }
 
-                if (requestBody.result.action) {
-                    console.log('requestBody.result.action->' + requestBody.result.action);
-                    if(requestBody.result.action == "horario.van")
-                    {                        
-                        var descOrigem = requestBody.result.parameters.origem;
-                        var descDestino = requestBody.result.parameters.destino;
+                //Ação para horario da Van    
+                if(requestBody.result.action && requestBody.result.action == "horario.van"){  
+                    console.log('acao de horario');                 
+                    var descOrigem = requestBody.result.parameters.origem;
+                    var descDestino = requestBody.result.parameters.destino;
+
+                    var ori = retornaCodigo(requestBody.result.parameters.origem);
+                    var dest = retornaCodigo(requestBody.result.parameters.destino);
+
+                    if(!(!ori || 0 === ori.length) && !(!dest || 0 === dest.length)){
+                        //  sync in node ¯\_(ツ)_/¯ but works !!!
+                        var resposta = syncrequest(
+                        'GET', 'https://vans.labbs.com.br/horario?idOrigem='+ ori +'&idDestino='+dest + '');
                         
-                        var ori = retornaCodigo(requestBody.result.parameters.origem);
-                        var dest = retornaCodigo(requestBody.result.parameters.destino);
-                        
-                        if(!(!ori || 0 === ori.length) && !(!dest || 0 === dest.length) ){
-                            var resposta = syncrequest(
-                            'GET',
-                            'https://vans.labbs.com.br/horario?idOrigem=1&idDestino=3'
-                            );
-                          
-                            var inform = JSON.parse(resposta.getBody());
-                            console.log('inform->'+ inform);    
-                          
-                            for(var i = 0; i < inform.length; i++) {
-                              speech +=inform[i]+ ' ';
-                            }
-                          
-                            //speech = JSON.parse(resposta.getBody());
-                            
-                            // Set the headers
-                          /*
-                            acessoAPIVan(ori,dest,function(entrada){
-                              res.json({
-                                        speech: speech,
-                                        displayText: speech,
-                                        source: 'bot-van-bb'
-                              });
-                              res.send();
-                           
-                            speech = getContent('https://vans.labbs.com.br/horario?idOrigem=1&idDestino=3')
-                              .then((html) => {
-                                console.log('##### RESPOSTA #####'+html);
-                                return html;
-                                })
-                              .catch((err) => console.error(err));
-                            */
-                            //console.log('esperarResultado->'+esperarResultado);
-                            console.log('possui resposta speech->'+ speech);                            
-                            //console.log('depois do wait speech->'+ speech);
-                            //console.log('esperarResultado->'+esperarResultado);                       
-                        } else {
-                          speech = 'Precisa de dois parametros';
-                          console.log('sem resposta speech->'+ speech);    
+                        var texto = '';
+                        var info = JSON.parse(resposta.getBody());
+
+                        console.log('info->'+ info);    
+
+                        for(var i = 0; i < info.length; i++) {
+                            texto +=info[i]+ ' ';
                         }
-                    }                    
+
+                        speech += 'Os horarios da van entre ' + descOrigem + ' para ' + descDestino + ' são ' + texto + '.';
+
+                        console.log('retorno->'+ speech);                            
+                    }
                 }
+
+                //Ação para obter localidades
+                if(requestBody.result.action && requestBody.result.action == "localidades.van"){ 
+                    console.log('acao de localidades');                 
+                    var resposta = syncrequest('GET', 'https://vans.labbs.com.br/localidades');
+                    var info = JSON.parse(resposta.getBody());
+                    var texto = '';
+
+                    if(info && info.length > 0){
+                        for(var i = 0; i < info.length; i++) {
+                            texto +=info[i].nome + ', ';
+                        }
+                        speech += 'Os locais atendidos pela Van são' + texto.substring(0, str.length - 2) + '.';
+                        console.log('retorno->'+ speech);    
+                    }
+                }
+
             }
         }
-       console.log('final');
-
         return res.json({
             speech: speech,
             displayText: speech,
             source: 'bot-van-bb'
         });
     } catch (err) {
-        console.error("Can't process request", err);
-
+        console.error("Não foi possivel obter a informação", err);
         return res.status(400).json({
             status: {
                 code: 400,
